97f49d8318ca3ffa661c8d3c6831abd503acc977
Vullum <markus.yung.vullum@gmail.com>
2023-02-23 18:00:33 +0100

    tester

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   const getRecipes = async() =>{
     //Pass på at ID og KEY kommer fra riktig app i edmam, hvis dere har registrert at dere skal bruke recipe api så må app id og key være koblet til det og ikke food databse eller omvendt
-    const response = await fetch('https://api.edamam.com/search?q=american&app_id=DIN_APP_ID&app_key=DIN_APP_KEY')
+    const response = await fetch('https://api.edamam.com/search?q=american&app_id=703571d9&app_key=ba5aea07aba20ea583a22cb6d771436d')
     const data = await response.json()
     console.log(data.hits)
     setRecipes(data.hits)
